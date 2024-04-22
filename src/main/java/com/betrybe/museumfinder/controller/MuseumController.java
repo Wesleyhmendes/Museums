098f12424ef5f61cf11addb6d1074d40409c7609
Wesley Mendes <wesleymendes123321@gmail.com>
2024-04-22 15:50:55 -0300

    Req 6

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -1,5 +1,14 @@
 package com.betrybe.museumfinder.controller;
 
+import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.service.MuseumServiceInterface;
+import com.betrybe.museumfinder.util.ModelDtoConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("/museums")
 public class MuseumController {
+  MuseumServiceInterface service;
+
+  @Autowired
+  public MuseumController(MuseumServiceInterface museumService) {
+    this.service = museumService;
+  }
+
+  /**
+   * Post museum.
+   */
+  @PostMapping
+  public ResponseEntity<Museum> createMuseum(@RequestBody MuseumDto museumDto) {
+    Museum convertedDto = ModelDtoConverter.dtoToModel(museumDto);
+    Museum newMuseum = service.createMuseum(convertedDto);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(newMuseum);
+  }
 
 }
